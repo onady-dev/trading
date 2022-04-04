277806fb96b930fd78f801bb849eef51fd8a353a
onady-dev <onady.dev@gmail.com>
2022-04-04 21:19:13 +0900

    연습 코드

---
+++ src/main/java/trading/trading/domain/Member.java
@@ -0,0 +1,24 @@
+package trading.trading.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    // 단축키 : 커맨드+n
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
